e3ac2a8339a09afc839a7f570b38fbe4be864ce7
Sirag Ahmed <ajj.siraj@gmail.com>
2020-04-10 00:15:36 +0200

    basic app structure

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import react from 'react';
+import {Nav, NavItem, Navbar, NavbarBrand, NavLink} from 'reactstrap';
+
+function Header(){
+    return(
+        <Nav>
+            
+        </Nav>
+    );
+}
