b82ff77cf79ecf748723ac6cccfab0d6d764a9eb
Sirag Ahmed <ajj.siraj@gmail.com>
2020-05-13 08:05:29 +0200

    search now fetches from api server

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -2,13 +2,14 @@ import React from "react";
 import { Col, Row, Container, Table } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import SearchBox from "./SearchBox";
-import {apiServerBaseUrl} from "../config";
+import { apiServerBaseUrl } from "../config";
 
 class SearchResults extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      searchTerm: ""
+      searchTerm: "",
+      searchResults: [],
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -19,47 +20,66 @@ class SearchResults extends React.Component {
     // fetch(`${apiServerBaseUrl}/search?q=${value}`)
     // .then((res) => res.json())
     // .then((res) => alert(JSON.stringify(res)));
-    
-    this.setState(prevState => ({...prevState, searchTerm: value}))
-    
+
+    this.setState((prevState) => ({ ...prevState, searchTerm: value }));
   }
 
-  componentDidMount(){
-    console.log("QUERY IN SEARCHRESULTS: ", this.props.match.match.params)
+  componentDidMount() {
+    this._isMounted = true;
+    console.log("QUERY IN SEARCHRESULTS: ", this.props.match.match.params);
+    let query = this.props.match.match.params.query;
+    console.log("QUERY: ", `${apiServerBaseUrl}/search?q=${query}`);
+    fetch(`${apiServerBaseUrl}/search?q=${query}`)
+      .then((res) => res.json())
+      // .then((res) => alert(JSON.stringify(res)));
+
+      .then(
+        (res) =>
+          this._isMounted &&
+          res.status == 200 &&
+          this.setState((prevState) => ({ ...prevState, searchResults: res.data }))
+      )
+      .catch((err) => console.error(err));
   }
+
+  componentWillUnmount() {
+    //fix memory leak warning
+    this._isMounted = false;
+  }
+
   render() {
-    console.log(this.state);
-    
-    let movies = [];
-    let searchTerm = this.state.searchTerm.trim().toLowerCase();
-    if (searchTerm.length > 0) {
-      movies = movies.filter(movie => movie.Title.toLowerCase().match(searchTerm));
-    }
-    
+    console.log("Search Results: ", this.state.searchResults);
+
+    let movies = this.state.searchResults;
+    // let searchTerm = this.state.searchTerm.trim().toLowerCase();
+    // if (searchTerm.length > 0) {
+    //   movies = movies.filter((movie) => movie.Title.toLowerCase().match(searchTerm));
+    // }
+
     let movieList = movies.map((movie, index) => {
-            return (
-              <tr key={`movie-list-table-${movie.imdbID}`}>
-                {/* <Link to={`/movies/${movie.imdbID}`}> */}
-                <td>
-                  <img
-                    className="img-fluid"
-                    src={movie.Poster}
-                    alt={movie.Title}
-                    style={{ maxHeight: "4rem" }}
-                  />
-                </td>
-                {/* </Link> */}
-                <td>{movie.Year}</td>
-                <td>{movie.Title}</td>
-                <td>{movie.imdbRating}</td>
-                <td>{movie.imdbVotes}</td>
-                <td>{movie.Country}</td>
-                <td>{movie.Genre}</td>
-                <td>{movie.Runtime}</td>
-                <td>{movie.Rated}</td>
-              </tr>
-            );
-          });
+      return (
+        <tr key={`movie-list-table-${movie.imdbID}`}>
+          {/* <Link to={`/movies/${movie.imdbID}`}> */}
+          <td>
+            <img
+              className="img-fluid"
+              src={movie.Poster}
+              alt={movie.Title}
+              style={{ maxHeight: "4rem" }}
+            />
+          </td>
+          {/* </Link> */}
+          <td>{movie.Year}</td>
+          <td>{movie.Title}</td>
+          <td>{movie.imdbRating}</td>
+          <td>{movie.imdbVotes}</td>
+          <td>{movie.Country}</td>
+          <td>{movie.Genre}</td>
+          <td>{movie.Runtime}</td>
+          <td>{movie.Rated}</td>
+        </tr>
+      );
+    });
     return (
       <Container>
         <Row>
@@ -94,7 +114,7 @@ class SearchResults extends React.Component {
                   <th>Rated</th>
                 </tr>
               </thead>
-              <tbody>{movieList}</tbody>
+              <tbody>{movieList.length == 0 ? <tr><td colSpan="9" style={{textAlign: 'center'}}>No Results.</td></tr> : movieList}</tbody>
             </Table>
           </Col>
         </Row>
@@ -104,4 +124,3 @@ class SearchResults extends React.Component {
 }
 
 export default SearchResults;
-
