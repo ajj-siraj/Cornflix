0bbe7aad7365c46746b3dbc149cc62f0bdbfc50d
Sirag Ahmed <ajj.siraj@gmail.com>
2020-06-22 23:11:42 +0200

    .gitignore

--- src/config.js
+++
@@ -1,5 +0,0 @@
-export const apiServerBaseUrl = "http://localhost:4000";
-
-export const recaptchaSiteKey = "6LcjovQUAAAAAFKvx0SeE-fSQYY4-KwrbwSLAlUk";
-
-export const newsApiKey = "0a95e66f29f94a6b8e72cb581d05d184";
